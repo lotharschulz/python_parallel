a39e5959f518daaac6386401cbf733550d0ddc8f
Lothar Schulz <lothar.sch@gmx.de>
2018-01-02 23:37:38 +0100

    init

---
+++ README.md
@@ -0,0 +1,16 @@
+# python parallel
+
+## set up
+```
+pip3 install pytest
+```
+
+## run
+```
+python test_python_parallel.py
+```
+
+## test
+```
+pytest -v
+```
---
+++ test_python_parallel.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+
+def my_func():
+    print("something")
+
+
+def test_my_func():
+    assert 3 == 3
+
+
+if __name__ == "__main__":
+    my_func()

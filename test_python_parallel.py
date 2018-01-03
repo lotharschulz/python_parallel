b9db9d2129aa541f7b3406fa87aa7d3372e391a9
Lothar Schulz <lothar.sch@gmx.de>
2018-01-03 01:58:03 +0100

    multi threading & processing started

--- test_python_parallel.py
+++ test_python_parallel.py
@@ -1,13 +1,71 @@
 #!/usr/bin/env python3
+import time
+from threading import Thread
+from multiprocessing import Process
 
 
-def my_func():
-    print("something")
+def fib(n):
+    curr, next_number, counter = 0, 1, 0
+    while True:
+        if counter >= n:
+            return
+        yield curr
+        curr, next_number = next_number, curr + next_number
+        counter += 1
 
 
-def test_my_func():
-    assert 3 == 3
+def collect_fibs(ns):
+    fibunaccies = fib(ns)
+    fibs_list = []
+    for fibunacci in fibunaccies:
+        fibs_list.append(fibunacci)
+    return fibs_list
+
+
+def test_fib():
+    fibs = fib(250000)
+    last_fib = 0
+    for fs in fibs:
+        last_fib = fs
+    assert 52247 >= len(str(last_fib))
+
+
+def test_collect_fibs():
+    fs = collect_fibs(250000)
+    assert 52247 >= len(str(fs[-1]))
 
 
 if __name__ == "__main__":
-    my_func()
+    n = 250000
+    print("sequential execution")
+    start = time.time()
+    f = fib(n)
+    fib_list = []
+    for x in f:
+        fib_list.append(x)
+    end = time.time()
+    print("len(fibList): {:d}, len(fibList[-1]): {:d}, time taken: {:f}".
+          format(len(fib_list), len(str(fib_list[-1])), end))
+
+    print("multi threading")
+    start = time.time()
+    p1 = Thread(target=fib, args=(n / 2,))
+    p1.start()
+    p2 = Thread(target=fib, args=(n / 2,))
+    p2.start()
+    # check return values
+    end = time.time()
+    print("time taken: {:f}".format(end))
+
+    print("multi processing")
+    start = time.time()
+    p1 = Process(target=fib, args=(n / 2,))
+    p1.start()
+    p2 = Process(target=fib, args=(n / 2,))
+    p2.start()
+    p1.join()
+    p2.join()
+    result = p1.exitcode == 0 and p2.exitcode == 0
+    # check return values
+    end = time.time()
+    print("successful: {}, time taken: {:f}".format(result, end))
